b7ef378d2a6485509804db4ee20b8e8261892253
Jaroslav Janda <janda.jaroslav5@gmail.com>
2020-02-19 11:49:08 +0100

    day 42

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Jaroslav Janda on 19/02/2020.
+//  Copyright © 2020 Jaroslav Janda. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
